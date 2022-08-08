9d35bec827552351f56aee0fb92375ec697fceff
Aya Ali <1aya.dn.ali@gmail.com>
2022-08-09 00:33:03 +0300

    finished the task

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+// var  bestCustomer = "not bob";
+const leastFavoriteCustomer = "billy";
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+  
+
+  
+  
+  function  changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy';
+
+  }
